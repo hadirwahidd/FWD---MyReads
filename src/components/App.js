69ac156a51732573aa8ae81ec950697f61805719
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-08-29 09:16:27 +0200

    added route path for 404 Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import "../css/App.css";
-//import { useState } from "react";
 import MainPage from "./MainPage.js";
 import SearchPage from "./SearchPage.js";
+import NotFound from "./NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-
-  //const [showSearchPage, setShowSearchpage] = useState(false);
 
   return (
     <div className="app">
@@ -14,6 +12,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<MainPage />} />
           <Route path="/search" element={<SearchPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
