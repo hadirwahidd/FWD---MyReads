bf7b623fd8e820e6c03452dc7b4d6f1d0c689dd1
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-08-29 09:17:30 +0200

    added component for 404 page

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import "../css/App.css";
+
+const NotFound = () => {
+    return (
+        <div className="notfound">
+            <h1>Error 404</h1>
+            <h3>Page Not Found!</h3>
+        </div>
+    )
+}
+
+export default NotFound;
