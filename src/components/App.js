8ddaa40604fe83fdf3f6e626f6bf16c552c65296
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-04 15:02:56 +0200

    useEffect & useState hooks

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,27 @@ import "../css/App.css";
 import MainPage from "./MainPage.js";
 import SearchPage from "./SearchPage.js";
 import NotFound from "./NotFound.js";
+import * as BooksAPI from "../utils/BooksAPI.js";
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const App = () => {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    };
+    getBooks();
+  }, []);
 
   return (
     <div className="app">
       <Routes>
         <Route exact path="/" element={<MainPage />} />
-        <Route path="/search" element={<SearchPage />} />
+        <Route path="/search" element={<SearchPage books={books}/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
