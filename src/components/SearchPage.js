a4af7ae598ffcbf4796532fabc6a541c075f6510
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-04 15:03:53 +0200

    useState hook

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,30 +1,66 @@
-//import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-//import * as BooksAPI from "../utils/BooksAPI.js";
+import Book from "./Book.js";
 
-const SearchPage = () => {
-    /*useEffect( () => {
-        const getAll = async () => {
-            const res = await BooksAPI.getAll();
-            return res;
-        };
+const SearchPage = ({ books }) => {
 
-        getAll();
-    }, []);*/
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (query) => {
+    setQuery(query);
+  };
+
+  const clearQuery = () => {
+    updateQuery("");
+  };
+
+  const showingBooks =
+    query === ""
+      ? books
+      : books.filter((b) =>
+        b.title.toLowerCase().includes(query.toLowerCase())
+      );
+
   return (
     <div className="search-books">
-          <div className="search-books-bar">
-            <Link to="/" className="close-search">
-              Close
-            </Link>
-            <div className="search-books-input-wrapper">
-              <input type="text" placeholder="Search by title, author, or ISBN" />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
+      <div className="search-books-bar">
+        <Link to="/" className="close-search">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={(event) => updateQuery(event.target.value)}
+          />
         </div>
+      </div>
+
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {showingBooks.map((book) => (
+            <li key={book.id}>
+              <Book bookTitle={book.title} bookAuthors={book.authors} ImageUrl={book.imageLinks.thumbnail} />
+            </li>
+          ))}
+        </ol>
+      </div>
+
+      {(showingBooks.length !== books.length && showingBooks.length !== 0) && (
+        <center>
+          <p>Now showing {showingBooks.length} of {books.length} books in the library</p>
+          <button onClick={clearQuery}>Show All</button>
+        </center>
+      )}
+
+      {showingBooks.length === 0 && (
+        <center>
+          <p>No results found</p>
+          <button onClick={clearQuery}>Show all books</button>
+        </center>
+      )}
+    </div>
   )
 }
 
