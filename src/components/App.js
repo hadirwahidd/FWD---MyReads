8f739f3defb3544dcb139df41117e46d71a5df3c
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-04 14:20:19 +0200

    modify App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
 import "../css/App.css";
 import MainPage from "./MainPage.js";
 import SearchPage from "./SearchPage.js";
-import NotFound from "./NotFound";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./NotFound.js";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
 
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<MainPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
