8a6e64686e0cc21667724966219e1c08ba1f64b8
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-16 23:42:37 +0200

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const App = () => {
-
+  
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
@@ -15,14 +15,23 @@ const App = () => {
       const res = await BooksAPI.getAll();
       setBooks(res);
     };
+
     getBooks();
   }, []);
+
+  /*const updateBooks = async (book, shelf) => {
+    await BooksAPI.update(book, shelf);
+  };
+
+  const updateShelf = async (book, shelf) => {
+    updateBooks(book, shelf);
+  };*/
 
   return (
     <div className="app">
       <Routes>
-        <Route exact path="/" element={<MainPage />} />
-        <Route path="/search" element={<SearchPage books={books}/>} />
+        <Route exact path="/" element={<MainPage books={books} />} />
+        <Route path="/search" element={<SearchPage books={books} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
