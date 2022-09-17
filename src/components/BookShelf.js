3e7df3a1e4dd3d35172b0a60cfee8dc9c8acf3bb
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-17 17:44:01 +0200

    passed updateShelf prop to Book component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,16 +1,18 @@
 import Book from "./Book";
 
-const BookShelf = ({ shelf, title, books }) => {
+const BookShelf = ({ shelf, title, books, updateShelf }) => {
     return (
         <div>
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {books.filter((book) => book.shelf === shelf).map((book) => (
+                        {books?.filter((book) => book.shelf === shelf).map((book) => (
                             <li key={book.id}>
-                                <Book bookTitle={book.title} bookAuthors={book.authors}
-                                    ImageUrl={book.imageLinks.thumbnail} Link={book.previewLink} />
+                                <Book shelf={book.shelf} updateShelf={(shelf) => updateShelf(book, shelf)}
+                                    bookTitle={book.title} bookAuthors={book.authors}
+                                    ImageUrl={book?.imageLinks?.thumbnail} Link={book.previewLink}
+                                />
                             </li>
                         ))}
                     </ol>
