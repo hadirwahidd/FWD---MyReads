809f16951c69b7403166f8757a2551b95cf08f15
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-17 17:44:46 +0200

    updated props of BookShelf Component

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import BookShelf from "./BookShelf";
 
-const MainPage = ({ books }) => {
+const MainPage = ({ books, updateShelf }) => {
     return (
         <div className="list-books">
             <div className="list-books-title">
                 <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-                <BookShelf shelf="currentlyReading" title="Currently Reading" books={books}/>
-                <BookShelf shelf="wantToRead" title="Want to Read" books={books}/>
-                <BookShelf shelf="read" title="Read" books={books}/>
+                <BookShelf shelf="currentlyReading" title="Currently Reading" books={books} updateShelf={updateShelf}/>
+                <BookShelf shelf="wantToRead" title="Want to Read" books={books} updateShelf={updateShelf}/>
+                <BookShelf shelf="read" title="Read" books={books} updateShelf={updateShelf}/>
             </div>
             <div className="open-search">
                 <Link to="/search">Add a book</Link>
