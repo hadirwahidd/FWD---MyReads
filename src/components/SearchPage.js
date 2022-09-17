3c81bbc576a626dfecf20bee20657f2b6dd1b66f
Hadir Wahid <96303782+hadirwahidd@users.noreply.github.com>
2022-09-17 23:53:09 +0200

    add shelf property to results array

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import Book from "./Book.js";
 import * as BooksAPI from "../utils/BooksAPI.js";
 
-const SearchPage = ({ shelf, updateShelf }) => {
+const SearchPage = ({ books, updateShelf }) => {
+
+  console.log(books);
 
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
@@ -20,9 +22,41 @@ const SearchPage = ({ shelf, updateShelf }) => {
       if (input !== "") {
         const res = await BooksAPI.search(query);
         if (res.error) {
+          console.log("Search Error");
           setResults([]);
         }
         else {
+          console.log(res);
+          const result = res.map((b) => {
+            books.find((book) => {
+              if (b.id === book.id) {
+                b.shelf = book.shelf;
+              }
+              else {
+                b.shelf = "none";
+              };
+            });
+          });
+          console.log(result);
+          /*const bookshelf = books.find(b => b.id === book.id);
+            if (bookshelf) {
+              book.shelf = bookshelf.shelf;
+            }
+            else {
+              book.shelf = "none";
+            };*/
+            
+          /*for (let i = 0; i < res.length; i++) {
+            console.log(res[i].id);
+            console.log(books[i].id);
+            if (res[i].id === books[i].id) {
+              console.log(res[i].id);
+              res[i].shelf = books[i].shelf;
+            }
+            else {
+              res[i].shelf = "none";
+            };
+          };*/
           setResults(res);
         }
       }
@@ -31,7 +65,7 @@ const SearchPage = ({ shelf, updateShelf }) => {
       }
     }
     catch (err) {
-      console.log("error");
+      console.log("Error:", err);
     }
   };
 
@@ -56,14 +90,22 @@ const SearchPage = ({ shelf, updateShelf }) => {
 
       <div className="search-books-results">
         <ol className="books-grid">
-          {showingResults.map((book) => (
-            <li key={book.id}>
-              <Book shelf={shelf} updateShelf={(shelf) => updateShelf(book, shelf)}
-                bookTitle={book.title} bookAuthors={book.authors}
-                ImageUrl={book?.imageLinks?.thumbnail} Link={book.previewLink}
-              />
-            </li>
-          ))}
+          {showingResults.map((book) => {
+            /*const bookshelf = books.find(b => b.id === book.id);
+            if (bookshelf) {
+              book.shelf = bookshelf.shelf;
+            }
+            else {
+              book.shelf = "none";
+            };*/
+            return (
+              <li key={book.id}>
+                <Book shelf={book.shelf} updateShelf={(shelf) => updateShelf(book, shelf)}
+                  bookTitle={book.title} bookAuthors={book.authors}
+                  ImageUrl={book?.imageLinks?.thumbnail} Link={book.previewLink}
+                />
+              </li>);
+          })}
         </ol>
       </div>
     </div>
